3fb86c6a76516824ff8a1ce8c5e6e0f9d7aaac3a
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-08-12 14:01:32 +0530

    firebase json added

--- src/cron job/firebaseNotification.cron.js
+++ src/cron job/firebaseNotification.cron.js
@@ -1,46 +1,46 @@
-// const Cart = require("../models/cart.model");
-// const cron = require("node-cron");
-// const { asyncHandler } = require("../utils/asyncHandler");
-// const { sendFirebaseNotification } = require("../utils/firebaseNotifier.utils");
+const Cart = require("../models/cart.model");
+const cron = require("node-cron");
+const { asyncHandler } = require("../utils/asyncHandler");
+const { sendFirebaseNotification } = require("../utils/firebaseNotifier.utils");
 
-// cron.schedule("* * * * *", async () => {
-//     try {
-//         console.log(`Firebase Notification Scheduler...`);
+cron.schedule("* * * * *", async () => {
+    try {
+        console.log(`Firebase Notification Scheduler...`);
 
-//         // Check if the user has items in their cart
-//         const userCartData = await Cart.find({
-//             "products.0": { $exists: true }, // Ensure the products array is not empty
-//         })
-//             .select("-createdAt -updatedAt -__v")
-//             .populate({ path: "userId", select: "firebaseToken" })
-//             .lean();
+        // Check if the user has items in their cart
+        const userCartData = await Cart.find({
+            "products.0": { $exists: true }, // Ensure the products array is not empty
+        })
+            .select("-createdAt -updatedAt -__v")
+            .populate({ path: "userId", select: "firebaseToken" })
+            .lean();
 
-//         if (userCartData.length === 0) {
-//             console.log("Cart not found with products...");
-//             return;
-//         }
-//         const userData = userCartData
-//             .map((cart) => cart.userId)
-//             .filter(
-//                 (user) =>
-//                     user !== null &&
-//                     user.firebaseToken !== null &&
-//                     user.firebaseToken !== undefined,
-//             );
+        if (userCartData.length === 0) {
+            console.log("Cart not found with products...");
+            return;
+        }
+        const userData = userCartData
+            .map((cart) => cart.userId)
+            .filter(
+                (user) =>
+                    user !== null &&
+                    user.firebaseToken !== null &&
+                    user.firebaseToken !== undefined,
+            );
 
-//         const userFirebaseToken = userData.map((user) => user.firebaseToken);
+        const userFirebaseToken = userData.map((user) => user.firebaseToken);
 
-//         console.log("tokens", userFirebaseToken);
+        console.log("tokens", userFirebaseToken);
 
-//         if (userFirebaseToken.length === 0) {
-//             console.log("No valid Firebase tokens found...");
-//             return;
-//         }
+        if (userFirebaseToken.length === 0) {
+            console.log("No valid Firebase tokens found...");
+            return;
+        }
 
-//         // Send notifications
-//         const data = await sendFirebaseNotification(userFirebaseToken);
-//         console.log(data);
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// });
+        // Send notifications
+        const data = await sendFirebaseNotification(userFirebaseToken);
+        console.log(data);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
--- src/utils/firebaseNotifier.utils.js
+++ src/utils/firebaseNotifier.utils.js
@@ -1,46 +1,46 @@
-// const admin = require("firebase-admin");
-// const serviceAccount = require("./stonks-b66d4-be8791d7d5c7.json");
+const admin = require("firebase-admin");
+const serviceAccount = require("../../firebase.json");
 
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
-// /**
-//  * Sends a notification to a list of registration tokens.
-//  *
-//  * @param {Array<string>} tokens - The list of device registration tokens.
-//  * @param {Object} message - The message payload.
-//  * @param {Object} options - The notification options.
-//  */
-// exports.sendFirebaseNotification = async (
-//     tokens,
-//     title = `🛒 Don't Forget Your Items!`,
-//     body = "You have some great items waiting in your cart. Complete your purchase now and enjoy! 🎉",
-// ) => {
-//     if (!tokens || tokens.length === 0) {
-//         console.log("No tokens provided for notification.");
-//         return;
-//     }
+/**
+ * Sends a notification to a list of registration tokens.
+ *
+ * @param {Array<string>} tokens - The list of device registration tokens.
+ * @param {Object} message - The message payload.
+ * @param {Object} options - The notification options.
+ */
+exports.sendFirebaseNotification = async (
+    tokens,
+    title = `🛒 Don't Forget Your Items!`,
+    body = "You have some great items waiting in your cart. Complete your purchase now and enjoy! 🎉",
+) => {
+    if (!tokens || tokens.length === 0) {
+        console.log("No tokens provided for notification.");
+        return;
+    }
 
-//     try {
-//         const options = {
-//             priority: "high",
-//             timeToLive: 60 * 60 * 24,
-//         };
-//         const message = {
-//             notification: {
-//                 title: title,
-//                 body: body,
-//                 sound: "default",
-//                 image: "https://api.dropeat.in/upload/dropit-logo.jpeg",
-//             },
-//             data: { key1: "value1", key2: "value2" },
-//         };
-//         const response = await admin
-//             .messaging()
-//             .sendToDevice(tokens, message, options);
-//         return { msg: "Notification sent successfully", response };
-//     } catch (error) {
-//         console.error("Error sending notification:", error);
-//     }
-// };
+    try {
+        const options = {
+            priority: "high",
+            timeToLive: 60 * 60 * 24,
+        };
+        const message = {
+            notification: {
+                title: title,
+                body: body,
+                sound: "default",
+                image: "https://api.dropeat.in/upload/dropit-logo.jpeg",
+            },
+            data: { key1: "value1", key2: "value2" },
+        };
+        const response = await admin
+            .messaging()
+            .sendToDevice(tokens, message, options);
+        return { msg: "Notification sent successfully", response };
+    } catch (error) {
+        console.error("Error sending notification:", error);
+    }
+};
