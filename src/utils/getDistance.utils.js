a41e4e3188464178174aa00b9b9a8f8a3f71e33a
developerakkoo <developerakshayjadhav@gmail.com>
2025-04-17 01:37:34 +0530

    get Distance method changes

--- src/utils/getDistance.utils.js
+++ src/utils/getDistance.utils.js
@@ -2,17 +2,16 @@ const axios = require("axios");
 
 const getDistance = async (userLat, userLon, shopLat, shopLon) => {
     const googleMapsApiKey = "AIzaSyADFvEEjDAljOg3u9nBd1154GIZwFWnono";
-    try {
-        const response = await axios.get(
-            `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${userLat},${userLon}&destinations=${shopLat},${shopLon}&key=${googleMapsApiKey}`,
-        );
-        const distanceInMeters =
-            response.data.rows[0].elements[0].distance.value;
-        return distanceInMeters / 1000; // Convert to kilometers
-    } catch (error) {
-        console.log(error);
-        throw new Error("Failed to calculate distance");
-    }
+    const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?` +
+        `units=metric&` +
+        `origins=${userLat},${userLon}&` +
+        `destinations=${shopLat},${shopLon}&` +
+        `key=${googleMapsApiKey}`,
+    );
+    const distanceInMeters =
+        response.data.rows[0].elements[0].distance.value;
+    return distanceInMeters / 1000; // Convert to kilometers
 };
 
 module.exports = { getDistance };
