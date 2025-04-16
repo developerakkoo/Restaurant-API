94b9de314600086d4bcf5766e1e7927e4513e11d
developerakkoo <developerakshayjadhav@gmail.com>
2025-04-17 01:49:06 +0530

    calculate distance error removed

--- src/utils/getDistance.utils.js
+++ src/utils/getDistance.utils.js
@@ -10,8 +10,22 @@ const getDistance = async (userLat, userLon, shopLat, shopLon) => {
         `key=${googleMapsApiKey}`,
     );
     const distanceInMeters =
-        response.data.rows[0].elements[0].distance.value;
-    return distanceInMeters / 1000; // Convert to kilometers
+    response.data.rows[0].elements[0].distance.value;;
+            
+          return distanceInMeters / 1000; // Convert to kilometers
 };
+
+getDistance(
+    "28.7041",
+    "77.1025",
+    "19.076",
+    "72.8777",
+)
+    .then((distance) => {
+        console.log("Distance in kilometers:", distance);
+    })
+    .catch((error) => {
+        console.error("Error fetching distance:", error);
+    });
 
 module.exports = { getDistance };
