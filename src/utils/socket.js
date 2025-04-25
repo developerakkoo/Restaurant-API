4ea433e5992d4fbb750b476dce76a545048362e7
developerakkoo <developerakshayjadhav@gmail.com>
2025-04-26 02:41:58 +0530

    new

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -15,7 +15,10 @@ module.exports = {
                 methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
                 credentials: true,
             },
-         
+            transports: ['websocket', 'polling'],
+            allowEIO3: true,
+            pingTimeout: 60000,
+            pingInterval: 25000,
         });
 
         return io;
